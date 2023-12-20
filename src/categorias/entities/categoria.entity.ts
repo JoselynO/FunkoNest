34caa18a90eeba015b093251bd86300f4e5a674f
Joselyn Obando <obandofernandez1@gmail.com>
2023-12-20 16:26:49 +0100

    Creando entities-categoria

--- src/categorias/entities/categoria.entity.ts
+++ src/categorias/entities/categoria.entity.ts
@@ -1,1 +1,23 @@
-export class Categoria {}
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { Funko } from '../../funkos/entities/funko.entity'
+
+@Entity({ name: 'categorias'})
+export class Categoria {
+  @PrimaryColumn({type: 'uuid'})
+  id: string;
+
+  @Column({type: 'varchar', length: 255, unique: true})
+  nombre: string;
+
+  @CreateDateColumn({name: 'created', type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP'})
+  createdAt: Date;
+
+  @UpdateDateColumn({name: 'updated', type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP'})
+  updatedAt: Date;
+
+  @Column({name: 'isDeleted', type: 'boolean', default: false})
+  isDeleted: boolean;
+
+  @OneToMany(() => Funko, (funko) => funko.categoria)
+  funkos: Funko[];
+}
