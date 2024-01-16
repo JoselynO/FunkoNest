92240b09ab068b44abec9b3916fee98c61953837
Joselyn Obando <obandofernandez1@gmail.com>
2024-01-16 20:51:54 +0100

    Creando pedidoService

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { PedidosService } from './pedidos.service';
 import { PedidosController } from './pedidos.controller';
+import { MongooseModule, SchemaFactory } from "@nestjs/mongoose";
+import { Pedido } from "./schemas/pedido.schema";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Funko } from "../funkos/entities/funko.entity";
+import * as mongoosePaginate from 'mongoose-paginate-v2'
+
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Pedido.name,
+        useFactory: () => {
+          const schema = SchemaFactory.createForClass(Pedido)
+          schema.plugin(mongoosePaginate)
+          return schema
+        },
+      },
+    ]),
+    TypeOrmModule.forFeature([Funko]),
+  ],
   controllers: [PedidosController],
   providers: [PedidosService],
 })
--- src/pedidos/pedidos.service.ts
+++ src/pedidos/pedidos.service.ts
@@ -1,20 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
-
+import { InjectModel } from "@nestjs/mongoose";
+import { Pedido, PedidoDocument } from "./schemas/pedido.schema";
+import { PaginateModel } from 'mongoose'
+import { InjectRepository } from "@nestjs/typeorm";
+import { Funko } from "../funkos/entities/funko.entity";
+import { Repository } from "typeorm";
+import { PedidosMapper } from "./mappers/pedidos.mapper";
+export const PedidosOrderByValues: string[] = ['_id', 'idUsuario'];
+export const PedidosOrderValues: string[] = ['asc', 'desc'];
 @Injectable()
 export class PedidosService {
+  private logger = new Logger(PedidosService.name);
+
+  constructor(
+    @InjectModel(Pedido.name)
+    private pedidosRepository: PaginateModel<PedidoDocument>,
+    @InjectRepository(Funko)
+    private readonly funkosRepository: Repository<Funko>,
+    private readonly pedidosMapper: PedidosMapper,
+  ) {
+  }
   create(createPedidoDto: CreatePedidoDto) {
     return 'This action adds a new pedido';
   }
 
-  findAll() {
-    return `This action returns all pedidos`;
+  async findAll(page: number, limit: number, orderBy: string, order: string) {
+    this.logger.log(`Buscando todos los pedidos con paginación y filtros: ${JSON.stringify({page, limit, orderBy, order,})}`,)
+    const options = { page, limit, sort:{[orderBy]: order,}, collection: 'es_ES',}
+    return await this.pedidosRepository.paginate({}, options);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pedido`;
+  async findOne(id: string) {
+   this.logger.log(`Buscando pedido con id ${id}`)
+    const pedidoEncontrado = await this.pedidosRepository.findById(id).exec()
+    if(!pedidoEncontrado){
+      throw new NotFoundException(`Pedido con id ${id} no encontrado`)
+    }
+    return pedidoEncontrado;
   }
+
+  as
 
   update(id: number, updatePedidoDto: UpdatePedidoDto) {
     return `This action updates a #${id} pedido`;
