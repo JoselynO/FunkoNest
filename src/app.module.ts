42608730c44fe3083548df6eef0a348e10c67940
Joselyn Obando <obandofernandez1@gmail.com>
2024-01-24 00:45:43 +0100

    Agregando perfiles .env.prod y modificando el package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 import { FunkosModule } from './funkos/funkos.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { ConfigModule } from '@nestjs/config';
 import { StorageModule } from './storage/storage.module';
 import { NotificationsModule } from './websockets/notifications/notifications.module';
 import { CacheModule } from '@nestjs/cache-manager';
@@ -9,11 +9,18 @@ import { DatabaseModule } from './config/database/database.module';
 import { PedidosModule } from './pedidos/pedidos.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { CorsConfigModule } from './config/cors/cors.module';
+import * as process from 'process'
 @Module({
   imports: [
+    ConfigModule.forRoot(
+      process.env.NODE_ENV === 'dev'
+        ? { envFilePath: '.env.dev' || '.env' }
+        : { envFilePath: '.env.prod' },
+    ),
+    CorsConfigModule,
     CacheModule.register(),
-    ConfigModule.forRoot(),
+    DatabaseModule,
     FunkosModule,
     CategoriasModule,
     StorageModule,
